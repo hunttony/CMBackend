c0841f40cf0905619d4a383dcf0c360e7d2fdd1c
hunttony <128110405+hunttony@users.noreply.github.com>
2024-06-24 10:05:50 -0500

    update to inxex.js

--- index.js
+++ index.js
@@ -9,10 +9,19 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your frontend's URL
+  origin: process.env.FRONTEND_URL, // Ensure this matches your frontend URL exactly
+  optionsSuccessStatus: 200,
 }));
 
-mongoose.connect(process.env.MONGO_URI);
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
 
 const AccessCodeSchema = new mongoose.Schema({
   code: String,
@@ -22,7 +31,6 @@ const AccessCodeSchema = new mongoose.Schema({
 const AccessCode = mongoose.model('AccessCode', AccessCodeSchema);
 
 app.use(bodyParser.json());
-
 
 paypal.configure({
   mode: 'sandbox', // or 'live' for production
@@ -47,6 +55,7 @@ app.post('/create-payment', (req, res) => {
 
   paypal.payment.create(create_payment_json, (error, payment) => {
     if (error) {
+      console.error('Error creating PayPal payment:', error);
       res.status(500).send(error);
     } else {
       res.json({ id: payment.id });
@@ -60,6 +69,7 @@ app.post('/execute-payment', (req, res) => {
 
   paypal.payment.execute(paymentId, execute_payment_json, async (error, payment) => {
     if (error) {
+      console.error('Error executing PayPal payment:', error);
       res.status(500).send(error);
     } else {
       const code = Math.random().toString(36).substr(2, 7);
@@ -73,7 +83,6 @@ app.post('/execute-payment', (req, res) => {
 
 app.get('/verify-code/:code', async (req, res) => {
   const { code } = req.params;
-  console.log('code: ',{code})
   const accessCode = await AccessCode.findOne({ code });
 
   if (accessCode && new Date() < new Date(accessCode.expiration)) {
@@ -83,6 +92,13 @@ app.get('/verify-code/:code', async (req, res) => {
   }
 });
 
+app.get('/generate-test-code', async (req, res) => {
+  const code = Math.random().toString(36).substr(2, 7);
+  const expiration = new Date(new Date().getTime() + 60 * 60 * 1000); // 1 hour from now
+  const newCode = new AccessCode({ code, expiration });
+  await newCode.save();
+  res.json({ code });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
