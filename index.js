5270c8e020d0a7eae283d32ece966276283e97c9
hunttony <128110405+hunttony@users.noreply.github.com>
2024-06-24 18:24:33 -0500

    cors update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // CORS configuration
-const allowedOrigins = ['http://localhost:5173', 'https://cm-frontend.vercel.app/'];
+const allowedOrigins = ['http://localhost:5173', 'https://cm-frontend.vercel.app'];
 app.use(cors({
   origin: function (origin, callback) {
     // Allow requests with no origin (like mobile apps or curl requests)
