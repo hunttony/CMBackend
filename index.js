31a143f3c5d7f8e20e2ea0e75fddc942a220bbcf
hunttony <128110405+hunttony@users.noreply.github.com>
2024-06-25 03:11:55 -0500

    chane port from 5000 to 3000

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const multerS3 = require('multer-s3');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // CORS configuration
 const allowedOrigins = ['http://localhost:5173', 'https://cm-frontend.vercel.app'];
